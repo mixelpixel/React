7d4d2cb15cc613eaec5addedcd4c09dc9e3506dc
mixelpixel <mixelpix@mac.com>
2017-06-27 04:12:54 -0400

    buttons with the text "NavBar Button"

---
+++ Lecture1/test/src/NavBarButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NavBarButton extends Component {
+  render() {
+    return (
+      <button>
+        NavBar Button
+      </button>
+    );
+  }
+}
