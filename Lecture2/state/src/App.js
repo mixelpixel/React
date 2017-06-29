8b1e67f6e9cc797dabcdb9777a6a489fc80ffe9a
mixelpixel <mixelpix@mac.com>
2017-06-28 20:58:30 -0400

    using function(props) for page title element

--- Lecture2/state/src/App.js
+++ Lecture2/state/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title={'My Functional Component'}/>
       </div>
     );
   }
