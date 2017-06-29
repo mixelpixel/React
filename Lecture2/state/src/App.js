bd1dcee7555e30f4de615fe17e99455f82bd170f
mixelpixel <mixelpix@mac.com>
2017-06-28 21:08:09 -0400

    formatting props

--- Lecture2/state/src/App.js
+++ Lecture2/state/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title={'My Functional Component'}/>
+        <Header title={'My Awesome React App'}/>
       </div>
     );
   }
--- Lecture2/state/src/Header.js
+++ Lecture2/state/src/Header.js
@@ -4,7 +4,7 @@ import './App.css';
 export default function(props) {
   return (
     <div className='Header'>
-      {props.title}
+      <h1>{props.title}</h1>
     </div>
   );
 }
