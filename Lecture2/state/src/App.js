d6d2342aa289a1e5ae7d06d56d7ca7a83a82a1cc
mixelpixel <mixelpix@mac.com>
2017-06-29 00:36:52 -0400

    Import Counter component

--- Lecture2/state/src/App.js
+++ Lecture2/state/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header title={'My Awesome React App'}/>
+        <Counter />
       </div>
     );
   }
