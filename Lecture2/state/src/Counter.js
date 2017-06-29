11159277ccd0ad27912756dfa0ed3f1838e837a4
mixelpixel <mixelpix@mac.com>
2017-06-29 01:05:12 -0400

    bind the incrementCount method

--- Lecture2/state/src/Counter.js
+++ Lecture2/state/src/Counter.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 export default class Counter extends Component {
   constructor(props) {
     super(props);
+    this.incrementCount = this.incrementCount.bind(this);
     this.state = {
       count: 0
     };
   }
 
+incrementCount() {
+  alert(this.state.count);
+}
+
   render () {
     return (
       <div>
         {this.state.count}
-        <button>Increment</button>
+        <button onClick={this.incrementCount} >Increment</button>
       </div>
     );
   }
