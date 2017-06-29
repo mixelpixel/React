ed01608b14a2774207a2823c8c615b3b1d0e9b18
mixelpixel <mixelpix@mac.com>
2017-06-28 20:35:47 -0400

    Header importing App.css

--- Lecture2/state/src/Header.js
+++ Lecture2/state/src/Header.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './App.css';
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <div className='Header'>
         Our Header
       </div>
     );
