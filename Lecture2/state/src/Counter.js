1e8ff03e72b4b0005c5074ef0556fda70545f1d0
mixelpixel <mixelpix@mac.com>
2017-06-29 00:49:30 -0400

    Constructor method to add an object for state changes

--- Lecture2/state/src/Counter.js
+++ Lecture2/state/src/Counter.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
   render () {
     const count = 0;
     return (
