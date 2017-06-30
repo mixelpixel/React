4384981c92a7ce3658c69463d1b3521d1051e571
mixelpixel <mixelpix@mac.com>
2017-06-30 13:32:31 -0400

    Counter

--- Lecture2/state/src/Counter.js
+++ Lecture2/state/src/Counter.js
@@ -10,16 +10,16 @@ export default class Counter extends Component {
   }
 
 incrementCount() {
-  // step 1
+  // // step 1:
   // alert(this.state.count);
-  // step 2
+  // // step 2:
   // this.state.count++;       // <--- view in browser JS console
   // console.log(this.state)   // <--- count increases, but not rendered!
-  //step 3
+  // // step 3:
   // this.setState({
   //   count: this.state.count +1
   // });
-  // step 4
+  // // step 4:
   this.setState(currentState => {   // <---- ASYNCHRONOUS!!! :D
     return {
       count: currentState.count + 1 // <--- better for instances when you need to reference
