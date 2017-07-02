40d9f7440c19df8b8ffaf2ef212fda41a69918eb
mixelpixel <mixelpix@mac.com>
2017-07-02 18:02:53 -0400

    Iterating over data array to create cards dynamically

--- Lecture3/feed/src/Feed.js
+++ Lecture3/feed/src/Feed.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react';
 import Card from './Card';
 import './App.css';
 
+// DUMMY DATAnormally grabbed through an http request or db or something
+const posts = [
+  {
+    title: 'My Post',
+    author: 'Ben',
+    content: 'osdhlajshdvlahdsjva ljdhlasd lsdhlajksdhfla liasdghf a'
+  },
+  {
+    title: 'My Post',
+    author: 'Ben',
+    content: 'osdhlajshdvlahdsjva ljdhlasd lsdhlajksdhfla liasdghf a'
+  },
+  {
+    title: 'My Post',
+    author: 'Ben',
+    content: 'osdhlajshdvlahdsjva ljdhlasd lsdhlajksdhfla liasdghf a'
+  },
+  {
+    title: 'My Post',
+    author: 'Ben',
+    content: 'osdhlajshdvlahdsjva ljdhlasd lsdhlajksdhfla liasdghf a'
+  }
+];
 
 export default class Feed extends Component {
 
   render() {
      return(
        <div className="feed">
-         <Card />
-         <Card />
-         <Card />
+         { posts.map((post) => {
+           return <Card />
+         }) }
        </div>
      )
    }
