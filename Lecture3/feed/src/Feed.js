6251a26f4f85e9de8e4157c2c903bab03e96c08d
mixelpixel <mixelpix@mac.com>
2017-07-02 17:08:31 -0400

    basic set up for card feed

---
+++ Lecture3/feed/src/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+export default class Feed extends Component {
+
+  render() {
+     return(
+       <div>
+
+       </div>
+     )
+   }
+}
