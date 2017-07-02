8461b7168e20f81ed786fdfde0385bd9b4f8bee5
mixelpixel <mixelpix@mac.com>
2017-07-02 17:17:43 -0400

    putting Card and Feed CSS in App.css

--- Lecture3/feed/src/Card.js
+++ Lecture3/feed/src/Card.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import './App.css';
+
 
 export default class Card extends Component {
   render() {
      return(
-       <div>
+       <div className="card">
          I'm a Card
        </div>
      )
--- Lecture3/feed/src/Feed.js
+++ Lecture3/feed/src/Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import './App.css';
+
 
 export default class Feed extends Component {
 
